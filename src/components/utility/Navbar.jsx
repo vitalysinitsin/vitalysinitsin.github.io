e0d9ba7b3553ef2d75c2f9460feb7971b0912229
vitaly <vsinitsin94@gmail.com>
2026-05-21 23:23:03 -0600

    Update package dependencies and comment out Navbar links for future adjustments

--- src/components/utility/Navbar.jsx
+++ src/components/utility/Navbar.jsx
@@ -1,8 +1,8 @@
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import Title from "./Title";
 
-const navLinkClass =
-  "flex h-full items-center text-nowrap px-4 text-lg font-bold cursor-pointer hover:bg-gray-100 hover:box-shadow-md transition-all duration-300";
+// const navLinkClass =
+//   "flex h-full items-center text-nowrap px-4 text-lg font-bold cursor-pointer hover:bg-gray-100 hover:box-shadow-md transition-all duration-300";
 
 function Navbar() {
   return (
