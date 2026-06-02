39fe394f227e070834164da1d06773c4930e2bae
vitaly <vsinitsin94@gmail.com>
2026-06-02 13:46:27 -0600

    changed title so forking doesnt make me look like I've made it haha

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -96,7 +96,7 @@ function AboutMe() {
           </p>
         </section>
         <section className="p-3 bg-white border-t w-full">
-          <span className="text-2xl font-extrabold">Latest Projects</span>
+          <span className="text-2xl font-extrabold">Latest Contributions</span>
           <div className="w-full h-1 bg-purple-300 my-2" alt="titleLine"></div>
           <div className="grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-2">
             {repos.map(
